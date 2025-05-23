a33e7ea2adc67a80b443424d1cd6e9b2b523a820
ztheleader <ztheleader@gmail.com>
2025-05-23 21:31:10 +0200

    feat: UI -> fetchNewFact() on the random fact page initialization.

--- src/app/features/random-fact/random-fact/random-fact.component.ts
+++ src/app/features/random-fact/random-fact/random-fact.component.ts
@@ -5,6 +5,7 @@ import { takeUntil } from 'rxjs/operators';
 
 import { Fact } from '../../../core/models/fact.model';
 import { FactService } from '../../../core/services/fact.service';
+import { FavoritesService } from '../../../core/services/favorites.service';
 
 @Component({
   selector: 'app-random-fact',
@@ -17,10 +18,14 @@ export class RandomFactComponent implements OnInit, OnDestroy {
   currentFact: Fact | null = null;
   isLoading: boolean = false;
   error: string | null = null;
+  isCurrentFactFavorite: boolean = false;
 
   private destroy$ = new Subject<void>();
+  private currentFactId: string | null = null;
 
-  constructor(private factService: FactService) {}
+  constructor(private factService: FactService,
+    private favoritesService: FavoritesService
+  ) { }
 
   ngOnInit(): void {
     this.factService.isLoading$.pipe(
@@ -35,12 +40,36 @@ export class RandomFactComponent implements OnInit, OnDestroy {
     ).subscribe(error => {
       this.error = error;
     });
+
+    this.fetchNewFact();
   }
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
     console.log('Random Fact Component Destroyed');
+  }
+
+  /**
+ * Fetches a new random fact from the API.
+ * Updates the component's current fact and checks its favorite status.
+ */
+  fetchNewFact(): void {
+    this.currentFact = null;
+    this.currentFactId = null;
+
+    // Call the service to get a random fact.
+    this.factService.getRandomFact()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (fact: Fact) => {
+          const factWithId: Fact = { ...fact, id: fact.id || this.favoritesService['generateUniqueId']() };
+          this.currentFact = factWithId;
+          this.currentFactId = factWithId.id; // Store the ID for easy favorite checking
+
+          this.isCurrentFactFavorite = this.favoritesService.isFavorite(this.currentFactId);
+        },
+      });
   }
 
 }
