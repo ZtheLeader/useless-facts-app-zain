10a75f566cb907ad902d595e692fd0fd5e2874e9
ztheleader <ztheleader@gmail.com>
2025-05-23 23:17:52 +0200

    feat - FavoritesListComponent:  Implement the component logic for favorites and hasFav

--- src/app/features/favorites/favorites-list/favorites-list.component.ts
+++ src/app/features/favorites/favorites-list/favorites-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+
+import { SavedFact } from '../../../core/models/fact.model';
+import { FavoritesService } from '../../../core/services/favorites.service';
 
 @Component({
   selector: 'app-favorites-list',
-  standalone: false,
   templateUrl: './favorites-list.component.html',
-  styleUrl: './favorites-list.component.scss'
+  standalone: false,
+  styleUrls: ['./favorites-list.component.scss']
 })
-export class FavoritesListComponent {
+export class FavoritesListComponent implements OnInit, OnDestroy {
+  public favorites$: Observable<SavedFact[]>;
+  public hasFavorites$: Observable<boolean>;
+  private destroy$ = new Subject<void>();
 
+  constructor(private favoritesService: FavoritesService) {
+    this.favorites$ = this.favoritesService.favorites$.pipe(takeUntil(this.destroy$));
+    this.hasFavorites$ = this.favorites$.pipe(
+      takeUntil(this.destroy$),
+      map(favorites => favorites.length > 0)
+    );
+  }
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onRemoveFavorite(factId: string): void {
+    this.favoritesService.removeFavorite(factId);
+  }
 }
