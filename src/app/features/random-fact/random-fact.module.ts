ffaffd2a6c0324f6fae50c4904cbd43e7c51e588
ztheleader <ztheleader@gmail.com>
2025-05-23 20:26:14 +0200

    feat: UI -> Import Material design related card for showing the fact.

--- src/app/features/random-fact/random-fact.module.ts
+++ src/app/features/random-fact/random-fact.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { RandomFactRoutingModule } from './random-fact-routing.module';
 import { RandomFactComponent } from './random-fact/random-fact.component';
 
-
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     RandomFactComponent
   ],
   imports: [
     CommonModule,
-    RandomFactRoutingModule
+    RandomFactRoutingModule,
+    MatCardModule
   ]
 })
 export class RandomFactModule { }
