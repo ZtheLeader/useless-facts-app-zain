4a8c057e65842f650439d273ed7459459ba359cc
ztheleader <ztheleader@gmail.com>
2025-05-23 21:44:48 +0200

    feat: UI -> Cleanup from app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-
-import { FactService } from './core/services/fact.service';
-import { Fact } from './core/models/fact.model';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,46 +6,8 @@ import { Subscription } from 'rxjs';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   title = 'useless-facts-app-zain';
 
-  private subscriptions: Subscription = new Subscription();
-
-  constructor(private factService: FactService) { }
-
-  ngOnInit(): void {
-    console.log('App Component Initialized');
-
-    this.subscriptions.add(
-      this.factService.isLoading$.subscribe(isLoading => {
-        console.log('FactService Loading State:', isLoading);
-      })
-    );
-
-    this.subscriptions.add(
-      this.factService.error$.subscribe(error => {
-        if (error) {
-          console.error('FactService Error State:', error);
-        } else {
-          console.log('FactService Error State: (none)');
-        }
-      })
-    );
-
-    this.subscriptions.add(
-      this.factService.getRandomFact().subscribe({
-        next: (fact: Fact) => {
-          console.log('Successfully fetched a random fact:', fact);
-          console.log('Fact text:', fact.text);
-        },
-        error: (err) => {
-          console.error('Error fetching fact in AppComponent:', err);
-        }
-      })
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
-  }
+  constructor() { }
 }
