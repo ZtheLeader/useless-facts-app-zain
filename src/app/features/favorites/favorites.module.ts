40372a690e2447658225ee4b18a156ed5d2cb1d8
ztheleader <ztheleader@gmail.com>
2025-05-23 22:04:21 +0200

    feat: UI -> Create search-input component.

--- src/app/features/favorites/favorites.module.ts
+++ src/app/features/favorites/favorites.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { FavoritesRoutingModule } from './favorites-routing.module';
 import { FavoritesListComponent } from './favorites-list/favorites-list.component';
 import { FavoriteItemComponent } from './favorite-item/favorite-item.component';
+import { SearchInputComponent } from './search-input/search-input.component';
 
 
 @NgModule({
   declarations: [
     FavoritesListComponent,
-    FavoriteItemComponent
+    FavoriteItemComponent,
+    SearchInputComponent
   ],
   imports: [
     CommonModule,
