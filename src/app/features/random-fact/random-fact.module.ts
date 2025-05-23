55fdaed38805e2dfa786241b65ed3cabb539dc22
ztheleader <ztheleader@gmail.com>
2025-05-23 20:17:23 +0200

    feat: UI -> Create component for random fact module.

--- src/app/features/random-fact/random-fact.module.ts
+++ src/app/features/random-fact/random-fact.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RandomFactRoutingModule } from './random-fact-routing.module';
+import { RandomFactComponent } from './random-fact/random-fact.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RandomFactComponent
+  ],
   imports: [
     CommonModule,
     RandomFactRoutingModule
---
+++ src/app/features/random-fact/random-fact/random-fact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random-fact',
+  standalone: false,
+  templateUrl: './random-fact.component.html',
+  styleUrl: './random-fact.component.scss'
+})
+export class RandomFactComponent {
+
+}
