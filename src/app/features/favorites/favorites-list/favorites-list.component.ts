5fc5cfabae89eb0df6eb396bd71152e4bd24d5b7
ztheleader <ztheleader@gmail.com>
2025-05-23 22:02:24 +0200

    feat: UI -> Create favorites-list component.

---
+++ src/app/features/favorites/favorites-list/favorites-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites-list',
+  standalone: false,
+  templateUrl: './favorites-list.component.html',
+  styleUrl: './favorites-list.component.scss'
+})
+export class FavoritesListComponent {
+
+}
--- src/app/features/favorites/favorites.module.ts
+++ src/app/features/favorites/favorites.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FavoritesRoutingModule } from './favorites-routing.module';
+import { FavoritesListComponent } from './favorites-list/favorites-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FavoritesListComponent
+  ],
   imports: [
     CommonModule,
     FavoritesRoutingModule
