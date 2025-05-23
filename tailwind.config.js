6dbbe274965319b61ab3ec793a4f4350d25a8026
ztheleader <ztheleader@gmail.com>
2025-05-23 12:57:12 +0200

    chore: Rollback to tailwindcss 3.x to fix the postcss issues.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}", // Important: This tells Tailwind where to find your HTML and TS files
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
