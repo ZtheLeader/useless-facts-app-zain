8e148cf7082e14303f3a1bbb90fe5de2515afe7f
ztheleader <ztheleader@gmail.com>
2025-05-23 22:34:32 +0200

    feat - SearchInputComponent: inject favorits service.

--- src/app/features/favorites/search-input/search-input.component.ts
+++ src/app/features/favorites/search-input/search-input.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { FavoritesService } from '../../../core/services/favorites.service';
+
 @Component({
   selector: 'app-search-input',
   standalone: false,
@@ -7,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './search-input.component.scss'
 })
 export class SearchInputComponent {
-
+  constructor(private favoritesService: FavoritesService) { } // Constructor is empty, but can be used for dependency injection if needed
 }
