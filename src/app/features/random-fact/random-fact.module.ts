da501be9d0e1a272f479a668b8ca804b39caf306
ztheleader <ztheleader@gmail.com>
2025-05-23 20:27:59 +0200

    feat: UI -> Import More Material design componets for fact related page.

--- src/app/features/random-fact/random-fact.module.ts
+++ src/app/features/random-fact/random-fact.module.ts
@@ -9,7 +9,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-
 @NgModule({
   declarations: [
     RandomFactComponent
