ba853747963a5534322a0a5b6d4c408f53af747c
ztheleader <ztheleader@gmail.com>
2025-05-23 12:51:54 +0200

    chore: Setup tailwindcss and global styles.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Important: This tells Tailwind where to find your HTML and TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
