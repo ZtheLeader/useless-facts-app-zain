d708df3c94872ff36b549758f262457f4e4fd970
ztheleader <ztheleader@gmail.com>
2025-05-23 20:06:59 +0200

    feat: Create random fact controller with its routes.

---
+++ src/app/features/random-fact/random-fact.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RandomFactRoutingModule } from './random-fact-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RandomFactRoutingModule
+  ]
+})
+export class RandomFactModule { }
