d201ad9972b968892cdf9ea532419fd52ee1e087
ztheleader <ztheleader@gmail.com>
2025-05-23 14:46:52 +0200

    chore: use angular 19 recomended way of using http provider.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,9 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
