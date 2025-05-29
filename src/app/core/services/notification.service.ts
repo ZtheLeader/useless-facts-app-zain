768e06865717196110d337f2778724d8e8f7aa50
ztheleader <ztheleader@gmail.com>
2025-05-29 23:57:42 +0200

    feat: Implement notification service.

--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -1,19 +1,67 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NotificationMessage } from '../models/notification.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
+  private duration = 3000;
 
-  constructor() {}
+  private notificationSubject = new Subject<NotificationMessage>();
+  public notification$ = this.notificationSubject.asObservable();
 
-  showError(message: string): void {
-    console.error(`Notification: ${message}`);
+  constructor(private snackBar: MatSnackBar) { }
+
+  showSuccess(message: string, duration?: number): void {
+    this.notificationSubject.next({
+      message,
+      type: 'success'
+    });
+
+    this.openSnackBar(message, 'Close', 'success-notification', duration);
   }
-  showSuccess(message: string): void {
-    console.log(`Notification: ${message}`);
+
+  showError(message: string, duration?: number): void {
+    this.notificationSubject.next({
+      message,
+      type: 'error'
+    });
+
+    this.openSnackBar(message, 'Close', 'error-notification', duration);
   }
-  showWarning(message: string): void {
-    console.warn(`Notification: ${message}`);
+
+  showWarning(message: string, duration?: number): void {
+    this.notificationSubject.next({
+      message,
+      type: 'warning'
+    });
+
+    this.openSnackBar(message, 'Close', 'warning-notification', duration);
+  }
+
+  showInfo(message: string, duration?: number): void {
+    this.notificationSubject.next({
+      message,
+      type: 'info'
+    });
+    this.openSnackBar(message, 'Close', 'info-notification', duration);
+  }
+
+
+  private openSnackBar(
+    message: string,
+    action: string,
+    panelClass: string,
+    duration?: number
+  ): void {
+
+    this.snackBar.open(message, action, {
+      duration: duration || this.duration,
+      panelClass: [panelClass],
+      verticalPosition: 'bottom',
+      horizontalPosition: 'center'
+    });
   }
 }
